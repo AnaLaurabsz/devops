fe953eec525fad299ac4d9b703b90c1586e54d71
AnaLaurabsz <analaura.b.souza1@gmail.com>
2025-11-21 14:36:18 -0300

    change logic tests

--- teste_app.py
+++ teste_app.py
@@ -11,20 +11,41 @@ class APITestCase(unittest.TestCase):
     def setUpClass(cls): 
         # Criação do cliente de teste 
         cls.client = app.test_client()
+        
+    def test_login_token_generation(self):
+        
+        response = self.client.post('/login') # Testa se a rota de login gera um token de acesso válido
+        self.assertEqual(response.status_code, 200)
+        data = response.json
+        self.assertIn('access_token', data)
+        
+        token = data['access_token'] # Verifica se o token é uma string não vazia
+        self.assertTrue(isinstance(token, str))
+        self.assertTrue(len(token) > 0)        
     
-    def test_home(self):
-        response = self.client.get('/')
+    
+    
+    def test_protected_route_message_content(self):  # Testa se o conteúdo da mensagem na rota protegida está correto.
+        
+        auth_response = self.client.post('/login')
+        access_token = auth_response.json['access_token']
+        
+        headers = {'Authorization': f'Bearer {access_token}'}
+        
+        response = self.client.get('/protected', headers=headers)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json, {"message": "API is running"})
 
-    def test_login(self):
-        response = self.client.post('/login')
+        esperado = "Protected route"
+        obtido = response.json['message']
+        
+        self.assertEqual(obtido, esperado, f"A mensagem deveria ser '{esperado}', mas veio '{obtido}'")
+        
+        
+    def test_swagger_ui_accessible(self):
+
+        response = self.client.get('/swagger/')
         self.assertEqual(response.status_code, 200)
-        self.assertIn('access_token', response.json)
-
-    def test_protected_no_token(self):
-        response = self.client.get('/protected')
-        self.assertEqual(response.status_code, 401)
+        self.assertIn(b'Swagger UI', response.data)
 
 if __name__ == '__main__':
     unittest.main()
